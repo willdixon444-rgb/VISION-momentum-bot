578aefebeb1471df79d09d6d6bd5fcb360fc539b
willdixon444-rgb <willdixon444@gmail.com>
2026-03-28 15:10:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -121,4 +121,4 @@ def test_scan():
 if __name__ == "__main__":
     engine.start()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=False)
